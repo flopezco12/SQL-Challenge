3ca07461541cbd20acc4a95c36be257d08cbc594
flopezco12 <141666916+flopezco12@users.noreply.github.com>
2023-10-23 19:07:11 -0400

    Update data analysis.sql

--- data analysis.sql
+++ data analysis.sql
@@ -6,9 +6,7 @@ ON s.emp_no = e.emp_no
 ORDER BY s.emp_no;
 
 /* 2.List the first name, last name, and hire date for the employees who were hired in 1986.*/
-SELECT e.emp_no, e.last_name, e.first_name, e.hire_date
-FROM employees AS e
-EXTRACT(YEAR FROM hire_date) = 1986;
+
 
 /* 3.List the manager of each department along with their department number, department name, employee number, last name, and first name.*/
 SELECT DISTINCT ON (dm.dept_no) dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
