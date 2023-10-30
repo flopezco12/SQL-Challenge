553091a174b6ac6d189e60ef6e93a73d4b126d34
flopezco12 <141666916+flopezco12@users.noreply.github.com>
2023-10-30 18:18:45 -0400

    Update table schemata.sql

--- table schemata.sql
+++ table schemata.sql
@@ -1,13 +1,79 @@
 -- SCHEMA: public
 
 -- DROP SCHEMA IF EXISTS public ;
+CREATE TABLE IF NOT EXISTS public.departments
+(
+    dept_no text COLLATE pg_catalog."default" NOT NULL,
+    dept_name text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT departments_pkey PRIMARY KEY (dept_no, dept_name)
+)
 
-CREATE SCHEMA IF NOT EXISTS public
-    AUTHORIZATION pg_database_owner;
+TABLESPACE pg_default;
 
-COMMENT ON SCHEMA public
-    IS 'standard public schema';
+ALTER TABLE IF EXISTS public.departments
+    OWNER to postgres;
 
-GRANT USAGE ON SCHEMA public TO PUBLIC;
+CREATE TABLE IF NOT EXISTS public.dept_emp
+(
+    emp_no integer NOT NULL,
+    dept_no text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT dept_emp_pkey PRIMARY KEY (emp_no, dept_no)
+)
 
-GRANT ALL ON SCHEMA public TO pg_database_owner;
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.dept_emp
+    OWNER to postgres;
+
+CREATE TABLE IF NOT EXISTS public.dept_manager
+(
+    dept_no text COLLATE pg_catalog."default" NOT NULL,
+    emp_no integer NOT NULL,
+    CONSTRAINT dept_manager_pkey PRIMARY KEY (dept_no, emp_no)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.dept_manager
+    OWNER to postgres;
+
+CREATE TABLE IF NOT EXISTS public.employees
+(
+    emp_no integer NOT NULL,
+    emp_title text COLLATE pg_catalog."default",
+    birth_date text COLLATE pg_catalog."default",
+    first_name text COLLATE pg_catalog."default",
+    last_name text COLLATE pg_catalog."default",
+    sex text COLLATE pg_catalog."default",
+    hire_date text COLLATE pg_catalog."default",
+    CONSTRAINT employees_pkey PRIMARY KEY (emp_no)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.employees
+    OWNER to postgres;
+
+CREATE TABLE IF NOT EXISTS public.salaries
+(
+    emp_no integer NOT NULL,
+    salary integer,
+    CONSTRAINT salaries_pkey PRIMARY KEY (emp_no)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.salaries
+    OWNER to postgres;
+
+CREATE TABLE IF NOT EXISTS public.titles
+(
+    title_id text COLLATE pg_catalog."default" NOT NULL,
+    title text COLLATE pg_catalog."default",
+    CONSTRAINT titles_pkey PRIMARY KEY (title_id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.titles
+    OWNER to postgres;
